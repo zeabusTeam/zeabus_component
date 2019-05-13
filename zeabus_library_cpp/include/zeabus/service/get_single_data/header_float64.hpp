daf7ec786b7e60a8483876486acfb62fbfba11a6
Supasan Komonlit <supasan_224@hotmail.com>
2019-05-14 00:25:29 +0700

    File service DepthCommand

---
+++ zeabus_library_cpp/include/zeabus/service/get_single_data/header_float64.hpp
@@ -0,0 +1,53 @@
+// FILE			: header_float64.hpp
+// AUTHOR		: K.Supasan
+// CREATE ON	: 2019, May 13 (UTC+0)
+// MAINTAINER	: K.Supasan
+
+// MACRO DETAIL
+
+// README
+
+// REFERENCE
+
+// MACRO SET
+
+#include    <zeabus/service/get_single_data/base_class.hpp>
+
+#include    <zeabus_utility/DepthCommand.h>
+
+#include    <zeabus_utility/HeaderFloat64.h>
+
+#ifndef _ZEABUS_SERVICE_GET_SINGLE_DATA_HEADER_FLOAT64_HPP
+#define _ZEABUS_SERVICE_GET_SINGLE_DATA_HEADER_FLOAT64_HPP
+
+namespace zeabus
+{
+
+namespace service
+{
+
+namespace get_single_data
+{
+
+    class HeaderFloat64 : public BaseClass< zeabus_utility::HeaderFloat64 >
+    {
+
+        public:
+            HeaderFloat64( std::shared_ptr< ros::NodeHandle > ptr_node_handle = NULL 
+                    , std::string frame_id = "filter/pressure" );
+
+        protected:
+            void ensure_setup_service( std::string service_topic );
+
+            bool callback( zeabus_utility::DepthCommand::Request& request 
+                    , zeabus_utility::DepthCommand::Response& response );
+
+    }; // class HeaderFloat64
+
+} // namespace get_single_data
+
+} // namespace service
+
+} // namespace zeabus
+
+#endif
---
+++ zeabus_library_cpp/src/zeabus/service/get_single_data/header_float64.cpp
@@ -0,0 +1,51 @@
+// FILE			: header_float64.cpp
+// AUTHOR		: K.Supasan
+// CREATE ON	: 2019, May 13 (UTC+0)
+// MAINTAINER	: K.Supasan
+
+// MACRO DETAIL
+
+// README
+
+// REFERENCE
+
+// MACRO SET
+
+#include    <zeabus/service/get_single_data/header_float64.hpp>
+
+namespace zeabus
+{
+
+namespace service
+{
+
+namespace get_single_data
+{
+
+    HeaderFloat64::HeaderFloat64( std::shared_ptr< ros::NodeHandle > ptr_node_handle 
+            , std::string frame_id ) : BaseClass( ptr_node_handle , frame_id )
+    {
+        ; 
+    } // constructor HeaderFloat64
+
+    void HeaderFloat64::ensure_setup_service( std::string service_topic )
+    {
+        this->service_server = this->ptr_node_handle->advertiseService( service_topic 
+                , &zeabus::service::get_single_data::HeaderFloat64::callback , this );
+    }
+
+    bool HeaderFloat64::callback( zeabus_utility::DepthCommand::Request& request
+            , zeabus_utility::DepthCommand::Response& response )
+    {
+        this->ptr_mutex_data->lock();
+        response.header = this->ptr_data->header;
+        response.depth = this->ptr_data->data;
+        this->ptr_mutex_data->unlock();
+        return true;
+    }
+
+} // namespace get_single_data
+
+} // namespace service
+
+} // namespace zeabus
