5dd449b91f023d50dba69298e02c555f74c3fd6d
Supasan Komonlit <supasan_224@hotmail.com>
2019-05-21 17:45:33 +0700

    Waiting toadd multi thread

---
+++ zeabus_library_cpp/include/zeabus/client/fusion_3_thread.hpp
@@ -0,0 +1,77 @@
+// FILE			: fusion_3_thread.hpp
+// AUTHOR		: K.Supasan
+// CREATE ON	: 2019, May 21 (UTC+0)
+// MAINTAINER	: K.Supasan
+
+// MACRO DETAIL
+
+// README
+//  This header will collect command to call 3 service in real time. What that mean?
+//  I will split thread to 3 thread to call all data 
+
+// REFERENCE
+
+// MACRO SET
+
+#include    <mutex>
+
+#include    <memory>
+
+#include    <iostream>
+
+#include    <ros/ros.h>
+
+#include    <geometry_msgs/Imu.h>
+
+#include    <zeabus_utility/HeaderFloat64.h>
+
+#include    <zeabus/client/base_class.hpp>
+
+#include    <geometry_msgs/Vector3Stamped.h>
+
+#include    <zeabus/single_thread/get_sensor_imu.hpp>
+
+#include    <zeabus/single_thread/get_depth_command.hpp>
+
+#include    <zeabus/single_thread/get_geometry_vector3_stamped.hpp>
+
+#ifndef _ZEABUS_CLIENT_FUSION_3_THREAD_HPP__
+#define _ZEABUS_CLIENT_FUSION_3_THREAD_HPP__
+
+namespace zeabus
+{
+
+namespace client
+{
+
+    class Fusion3Thread : zeabus::client::base_class< 3 >
+    {
+        public:
+            Fusion3Thread( std::shared_ptr< ros::NodeHandle > ptr_node_handle );
+
+            void setup_ptr_mutex_data( std::shared_ptr< std::mutex > dvl_mutex 
+                    , std::shared_ptr< std::mutex > imu_mutex 
+                    , std::shared_ptr< std::mutex > pressure_mutex );
+
+            void setup_client( std::string* dvl_topic , std::string* imu_topic 
+                    , std::string* pressure_topic );
+
+            void setup_data( geometry_msgs::Vector3Stamped* dvl_data , sensor_msgs::Imu* imu_data
+                    , zeabus_utility::HeaderFloat64* pressure_data );
+
+            void all_call();
+
+        protected:
+            zeabus::client::single_thread::GetDepthCommand pressure_client;
+            zeabus::client::single_thread::GetGeometryVector3Stamped dvl_client;
+            zeabus::client::single_thread::GetSensorImu imu_client;
+            geometry_msgs::Vector3Stamped* dvl_data;
+            sensor_msgs::Imu* imu_data;
+            zeabus_utility::HeaderFloat64* pressure_data;
+    };
+
+} // namespce client
+
+} // namespace zeabus
+
+#endif // _ZEABUS_CLIENT_FUSION_3_THREAD_HPP__
