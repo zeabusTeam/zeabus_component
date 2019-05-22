a51110c023358201a6383b771891afe824aaaf99
Supasan Komonlit <supasan_224@hotmail.com>
2019-05-22 21:06:42 +0700

    Add library for ros::Time string

--- zeabus_library_cpp/include/zeabus/ros_interfaces/time.hpp
+++ zeabus_library_cpp/include/zeabus/ros_interfaces/time.hpp
@@ -6,10 +6,37 @@
 // MACRO DETAIL
 
 // README
-//  This will you to help about time stamp to manage and use to warn you about same time stamp
+//  This will apply ros::Time
 
 // REFERENCE
 //  ref1    : https://en.cppreference.com/w/cpp/language/storage_duration 
 //  ref2    : https://en.cppreference.com/w/cpp/language/static
 
 // MACRO SET
+
+#include    <ros/time.h>
+
+#include    <zeabus/convert/to_string.hpp>
+
+#ifndef _ZEABUS_ROS_INTERFACES_TIME_HPP__
+#define _ZEABUS_ROS_INTERFACES_TIME_HPP__
+
+namespace zeabus
+{
+
+namespace ros_interfaces
+{
+
+namespace time
+{
+
+    // will use ros::Time::now() to convert to string and return result
+    std::string time_string();
+
+} // namespace time
+
+} // namespace ros_interfaces
+
+} // namespace zeabus
+
+#endif
