015383a3e56e360aa9edc2a422a07916cd9b6b4b
Supasan Komonlit <supasan_224@hotmail.com>
2019-05-25 11:09:39 +0700

    Add new library

---
+++ zeabus_library_cpp/include/zeabus/service/control_command.hpp
@@ -0,0 +1,57 @@
+// FILE			: control_command.hpp
+// AUTHOR		: K.Supasan
+// CREATE ON	: 2019, May 24 (UTC+0)
+// MAINTAINER	: K.Supasan
+
+// MACRO DETAIL
+
+// README
+
+// REFERENCE
+//  ref1    : http://wiki.ros.org/msg
+
+// MACRO SET
+
+// MARCO CONDITION
+
+#include    <zeabus/service/base_class.hpp>
+
+#include    <zeabus_utility/SendControlCommand.h>
+
+#ifndef _ZEABUS_SERVICE_CONTROL_COMMAND_HPP__
+#define _ZEABUS_SERVICE_CONTROL_COMMAND_HPP__
+
+namespace zeabus
+{
+
+namespace service
+{
+
+    class ControlCommand : public zeabus::service::BaseClass
+    {
+        public:
+            ControlCommand( std::shared_ptr< ros::NodeHandle > ptr_node_handle );
+        
+            bool setup_server_service( std::string service_topic );
+
+            bool setup_ptr_data( zeabus_utility::ControlCommand* ptr_control_command );
+
+        protected:
+            bool callback( zeabus_utility::SendControlCommand::Request& request 
+                    , zeabus_utility::SendControlCommand::Response& response );
+
+            zeabus_utility::ControlCommand* ptr_control_command;
+
+            bool already_setup_ptr_data;
+
+            ros::ServiceServer server_service;
+
+
+        
+    }; // object ControlCommand
+
+} // namespace service
+
+} // namespace zeabus
+
+#endif
---
+++ zeabus_library_cpp/src/zeabus/service/control_command.cpp
@@ -0,0 +1,80 @@
+// FILE			: control_command.cpp
+// AUTHOR		: K.Supasan
+// CREATE ON	: 2019, May 25 (UTC+0)
+// MAINTAINER	: K.Supasan
+
+// MACRO DETAIL
+
+// README
+
+// REFERENCE
+
+// MACRO SET
+
+// MARCO CONDITION
+
+#include    <zeabus/service/control_command.hpp>
+
+namespace zeabus
+{
+
+namespace service
+{
+
+    ControlCommand::ControlCommand( std::shared_ptr< ros::NodeHandle > ptr_node_handle )
+            : BaseClass( ptr_node_handle )
+    {
+        this->already_setup_ptr_data = false;
+    } // function structure ControlCommand
+
+    bool ControlCommand::setup_server_service( std::string service_topic )
+    {
+        bool result = true;
+        if( ! (this->already_setup_ptr_data ) )
+        {
+            std::cout   << zeabus::escape_code::bold_red 
+                        << "Service of control can't setup please setup_ptr_data\n"
+                        << zeabus::escape_code::normal_white;
+            result = false;
+        }
+        if( ! ( this->already_setup_ptr_mutex_data ) )
+        {
+            std::cout   << zeabus::escape_code::bold_red
+                        << "Service of control can't setup please setup_ptr_mutex_data\n"
+                        << zeabus::escape_code::normal_white;
+            result = false;
+        }
+        if( ! ( this->already_setup_ptr_node_handle ) )
+        {
+            std::cout   << zeabus::escape_code::bold_red
+                        << "Service of control can't setup please setup_ptr_node_handle\n"
+                        << zeabus::escape_code::normal_white;
+            result = false;
+        }
+        if( result )
+        {
+            this->server_service = this->ptr_node_handle->advertiseService( service_topic 
+                , &zeabus::service::ControlCommand::callback , this );
+        }
+        return result;
+    } // function setup_server_service
+    
+    bool ControlCommand::setup_ptr_data( zeabus_utility::ControlCommand* ptr_control_command )
+    {
+        this->ptr_control_command = ptr_control_command;
+        this->already_setup_ptr_data = true;
+        return true;
+    } // function setup_ptr_data
+
+    bool ControlCommand::callback( zeabus_utility::SendControlCommand::Request& request
+            , zeabus_utility::SendControlCommand::Response& response )
+    {
+        this->ptr_mutex_data->lock();
+        *(this->ptr_control_command) = request.command;
+        return true;
+    } // function callback
+
+
+} // namespace service
+
+} // namespace zeabus
