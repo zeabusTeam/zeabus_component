d131780018c568db6a85a09402c21bceec43cfd3
Supasan Komonlit <supasan_224@hotmail.com>
2019-05-31 09:33:12 +0700

    change name imu

--- zeabus_sensor/src/imu_node.cpp
+++ zeabus_sensor/src/imu_node.cpp
@@ -59,7 +59,7 @@ namespace IMUProtocal = zeabus::sensor::IMU::LORD_MICROSTRAIN;
 
 int main( int argv , char** argc )
 {
-    zeabus::sensor::IMU::Connector imu("/dev/microstrain/3dm_gx5_45_0000__6251.65903" , 100 );
+    zeabus::sensor::IMU::Connector imu("/dev/microstrain/3dm_gx5_45_0000__6251.65901" , 100 );
 
 #ifdef _IMU_NED_MODE_
     std::string node_name = "imu_node_ned";
