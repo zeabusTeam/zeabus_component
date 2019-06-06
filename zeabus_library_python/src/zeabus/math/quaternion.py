ac68a5b83950426a98b125b8bcef222c45f87829
Supasan Komonlit <supasan_224@hotmail.com>
2019-06-06 07:24:59 +0700

    Try to start quaternion library

---
+++ zeabus_library_python/src/zeabus/math/quaternion.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# FILE			: quaternion.py
+# AUTHOR		: K.Supasan
+# CREATE ON		: 2019, June 05 (UTC+0)
+# MAINTAINER	: K.Supasan
+
+# README
+#   This code have inspiration to create from use python quaternion to distribute data between
+#       parent frame and child frame
+
+# REFERENCE
+#   ref01 : http://docs.ros.org/melodic/api/tf/html/python/transformations.html#module-tf.transform
+#   ref02 : https://docs.python.org/2/tutorial/classes.html
+
+import math
+from tf import transformations as tf_handle
+
+class Quaternion
+
+    def
