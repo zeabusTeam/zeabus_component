11d45ce893dcf9b87bb22e352cff8ab209f87487
Supasan Komonlit <supasan_224@hotmail.com>
2019-06-13 23:12:53 +0700

    Updated header file

--- zeabus_library_cpp/include/zeabus/fuzzy/control_error_3dimension.hpp
+++ zeabus_library_cpp/include/zeabus/fuzzy/control_error_3dimension.hpp
@@ -27,11 +27,18 @@
 
 #include    <iostream>
 
-#include    <ros/ros.h>
+#include    <ros/console.h>
+
+#include    <ros/publisher.h>
+
+#include    <ros/node_handle.h>
 
 #include    <zeabus/escape_code.hpp>
 
 #include    <zeabus_utility/ControlFuzzy.h>
+
+#ifndef _ZEABUS_FUZZY_CONTROL_ERROR_3DIMENSION_HPP__
+#define _ZEABUS_FUZZY_CONTROL_ERROR_3DIMENSION_HPP__
 
 namespace zeabus
 {
@@ -44,7 +51,8 @@ namespace fuzzy
 
         public:
             // first argument std::string topic_name will use to make topic publish data
-            Control3Dimension( ros::NodeHandle* ptr_node_handle , std::string topic_name );
+            ControlError3Dimension( ros::NodeHandle* ptr_node_handle , std::string topic_name 
+                , std::string frame_name );
             
             // this use set offset value will plus in last result of relative value output
             void set_offset( double offset = 0 );
@@ -74,16 +82,19 @@ namespace fuzzy
         protected:
 
             // Below 4 variable will collect ptr of about fuzzification and fuzzy rule
-            std::array< double , 3 >* ptr_error_rule;
-            std::array< double , 3 >* ptr_diff_rule;
-            std::array< double , 3 >* ptr_force_rule;
-            std::array< double , 3 >* ptr_defuzzy_rule;
+            std::array< double , 3 >* ptr_error_rule; // rule for fuzzification error range
+            std::array< double , 3 >* ptr_diff_rule; // rule for fuzzification diff range
+            // force rule will have to 4 part because we want to get value for bound force
+            std::array< double , 4 >* ptr_force_rule; // rule for fuzzification force range
+            std::array< double , 3 >* ptr_defuzzy_rule; // rule for defuzzification relative 
             std::array< std::array < std::array < int , 7 > , 7 > , 7 >* ptr_fuzzy_rule;
 
             // Use to publish data
             // This message_pub will use to calculate all data in fuzzy too
             ros::Publisher fuzzy_pub;
             zeabus_utility::ControlFuzzy message_pub;
+
+            double offset;
 
         private:
             // use to calculate about diff of error ( you can think that it similar velocity )
@@ -96,11 +107,17 @@ namespace fuzzy
             void fuzzy_rule();
 
             // this function will extract of output and will publish data
+            void defuzzification();
+
+            //  this is last function will get last result and will publish all data
             //  Warning output will plus offset before
-            double defuzzification();
-            
+            double last_result();
+           
+            std::string my_name; 
     }; // object Control3Dimension
 
 } // namespace fuzzy
 
 } // namespace zeabus
+
+#endif // _ZEABUS_FUZZY_CONTROL_ERROR_3DIMENSION_HPP__
