a0d8c9eb7cc9653bd753e12be6696aaeab29cb3b
oomsarita <oomsarita@gmail.com>
2019-06-18 14:15:33 +0700

    edit fuzzy

--- zeabus_library_cpp/src/zeabus/fuzzy/control_fuzzy.cpp
+++ zeabus_library_cpp/src/zeabus/fuzzy/control_fuzzy.cpp
@@ -276,7 +276,7 @@ int crisp_to_output_rpy(int fuzzy_value)
     return num;
 }
 
-void print(int type, int output)
+void print(int type, int output, int crisp, int diff)
 {
     // std::string x,y,z,r,p,y;
     // std::string axis[6] = {x,y,z,r,p,yaw};
@@ -306,13 +306,14 @@ void print(int type, int output)
         std::cout << "axis : yaw";
     }
     // std::cout << std::endl;
-    std::cout << "  ";
+    std::cout << std::endl;
+    std::cout << "input error : " << crisp << std::endl;
+    std::cout << "diff error : " << diff << std::endl;
     std::cout << "output force : " << output << std::endl;
 }
 
 int run_system(double crisp,int type)
 {
-    std::cout << "input error : " << crisp << std::endl;
     int current_error;
     int value_of_diff;
     int change_error;
@@ -327,9 +328,12 @@ int run_system(double crisp,int type)
     {
         current_error = crisp_to_error_rpy(crisp);
     }
+    std::cout << "current error : " << current_error << std::endl;
+    std::cout << "previous error : " << previous_error[type] << std::endl;
     check_error(current_error, previous_error[type]);
     previous_error[type] = current_error;
     value_of_diff = diff(value1, value2);
+    // std::cout << value1 << " " << value2 << std::endl;
     change_error = change_to_fuzzy(current_error);
     change_diff = change_to_fuzzy(value_of_diff);
     output_fuzzy = fuzzy_rule(change_diff, change_error, previous_output[type]);
@@ -350,6 +354,6 @@ int run_system(double crisp,int type)
     {
         output = crisp_to_output_rpy(output_fuzzy);
     }
-    print(type, output);
+    print(type, output, crisp, value_of_diff);
     return output;
 }
