8283882ca3d288ecca826feb895d94cc861438a3
Supasan Komonlit <supasan_224@hotmail.com>
2019-06-22 13:36:10 +0700

    Add header file

---
+++ zeabus_control/include/zeabus_control/fuzzy/pitch_parameter.hpp
@@ -0,0 +1,34 @@
+// FILE			: pitch_parameter.hpp
+// AUTHOR		: K.Supasan
+// CREATE ON	: 2019, June 22 (UTC+0)
+// MAINTAINER	: K.Supasan
+
+// MACRO DETAIL
+
+// README
+
+// REFERENCE
+
+// MACRO SET
+
+// MACRO CONDITION
+
+#ifndef _ZEABUS_CONTROL_FUZZY_ROLL_PARAMETER_HPP
+#define _ZEABUS_CONTROL_FUZZY_ROLL_PARAMETER_HPP
+
+namespace zeabus_control
+{
+
+namespace fuzzy
+{
+
+namespace pitch_parameter
+{
+
+} // namespace roll_parameter
+
+} // namespace fuzzy 
+
+} // namespace zeabus_control
+
+#endif // _ZEABUS_CONTROL_ROLL_PARAMETER_HPP
