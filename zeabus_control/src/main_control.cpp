8c38a6cc3d3324a5e2062bb5e82687d1b7bfc5ba
Supasan Komonlit <supasan_224@hotmail.com>
2019-06-23 01:02:15 +0700

    Start nw control & last?

---
+++ zeabus_control/src/main_control.cpp
@@ -0,0 +1,15 @@
+// FILE			: main_control.cpp
+// AUTHOR		: K.Supasan
+// CREATE ON	: 2019, June 22 (UTC+0)
+// MAINTAINER	: K.Supasan
+
+// MACRO DETAIL
+
+// README
+//  This is the lastest fuzzy control on ZEABUS Team. This version 2
+
+// REFERENCE
+
+// MACRO SET
+
+// MACRO CONDITION
