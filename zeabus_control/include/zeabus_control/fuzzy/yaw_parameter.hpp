e081594c74e909dcfb0c342d42c8983608138689
Supasan Komonlit <supasan_224@hotmail.com>
2019-06-22 21:07:52 +0700

    Add header of all parameter

--- zeabus_control/include/zeabus_control/fuzzy/yaw_parameter.hpp
+++ zeabus_control/include/zeabus_control/fuzzy/yaw_parameter.hpp
@@ -24,6 +24,37 @@ namespace fuzzy
 
 namespace yaw_parameter
 {
+    // Error is range of error to decision 0 1 2 3 by use 3 value. 
+    // Data is vector type
+    const static std::array< double , 3 > ERROR_RULE = { 0.1 , 1 , 2 };
+
+    // Diff is range to decision about velocity error will be 0 1 2 3 by use 3 value
+    // Data is vertor type
+    const static std::array< double , 3 > DIFF_RULE = { 0.03 , 0.25 , 0.60 };
+
+    // Force is range to decision now you have active force what range
+    // member 3 (indent 0) use to decision that is limit of force we can do
+    //  Output will decision to 0 1 2 3 4 by use 4 value
+    //  If over member 3 (indent 0) we will not addition force
+    //  Data is vector type
+    const static std::array< double , 4 > FORCE_RULE = { 0.1 , 0.3 , 2 , 3 };
+
+    // About condition to convert crisp_set to fuzzy_set we will use this condition
+    //  if( abs( intput ) < rule[0] ) output = 0
+    //  else if( abs(input) < rule[1] ) output = 1
+    //  else if( abs(input) < rule[2] ) output = 2
+    //  else output = 3 
+
+    // Defuzzy is value ralative to convert from crisp set to fuzzy data
+    // Data will have 0, 1, 2, 3, 4.
+    // If output is 0 will add 0
+    // If output is 1 2 will add by DEFUZZY_RULE member 0 1
+    // If output is 3 4 will add by DEFUZZY_RULE member 2
+    // Data is verctor type
+    const static std::array< double , 3 > DEFUZZY_RULE = {0.02 , 0.08 , 0.4 };
+
+    // Offset is value to do and don't have affect with robot in real time always
+    const static double OFFSET = 0.0;
 
 } // namespace roll_parameter
 
