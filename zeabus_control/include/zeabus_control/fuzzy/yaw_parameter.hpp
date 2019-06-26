01050d4957d549503196b783e01f54363b377689
Robot <zeabus.auv@gmail.com>
2019-06-26 19:33:50 +0700

    Edit parameter fuzzy

--- zeabus_control/include/zeabus_control/fuzzy/yaw_parameter.hpp
+++ zeabus_control/include/zeabus_control/fuzzy/yaw_parameter.hpp
@@ -30,14 +30,14 @@ namespace yaw_parameter
     
     // Diff is range to decision about velocity error will be 0 1 2 3 by use 3 value
     // Data is vertor type
-    const std::array< double , 3 > DIFF_RULE = { 0.03 , 0.25 , 0.60 };
+    const std::array< double , 3 > DIFF_RULE = { 0.01 , 0.05 , 0.1 };
     
     // Force is range to decision now you have active force what range
     // member 3 (indent 0) use to decision that is limit of force we can do
     //  Output will decision to 0 1 2 3 4 by use 4 value
     //  If over member 3 (indent 0) we will not addition force
     //  Data is vector type
-    const std::array< double , 4 > FORCE_RULE = { 0.1 , 0.3 , 2 , 3 };
+    const std::array< double , 4 > FORCE_RULE = { 0.02 , 0.1 , 2 };
     
     // About condition to convert crisp_set to fuzzy_set we will use this condition
     //  if( abs( intput ) < rule[0] ) output = 0
@@ -51,7 +51,7 @@ namespace yaw_parameter
     // If output is 1 2 will add by DEFUZZY_RULE member 0 1
     // If output is 3 4 will add by DEFUZZY_RULE member 2
     // Data is verctor type
-    const std::array< double , 3 > DEFUZZY_RULE = {0.02 , 0.08 , 0.4 };
+    const std::array< double , 3 > DEFUZZY_RULE = {0.01 , 0.04 , 0.08 };
     
     // Offset is value to do and don't have affect with robot in real time always
     const double OFFSET = 0.0;
