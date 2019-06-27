fea5b28451da570c2d6cad5027a696f2e95ed85b
Supasan Komonlit <supasan_224@hotmail.com>
2019-06-27 10:35:22 +0700

    Start lib part receive subscribe

---
+++ zeabus_library_cpp/include/zeabus/subscribe/mission_planner_switch.h
@@ -0,0 +1,24 @@
+// FILE			: mission_planner_switch.h
+// AUTHOR		: K.Supasan
+// CREATE ON	: 2019, June 26 (UTC+0)
+// MAINTAINER	: K.Supasan
+
+// MACRO DETAIL
+
+// README
+
+// REFERENCE
+
+// MACRO SET
+
+// MACRO CONDITION
+
+#include    <ros/ros.h>
+
+#include    <zeabus_utility/MessagePlannerSwitch.h>
+
+#ifndef _ZEABUS_LIBRARY_SUBSCRIBER_MESSAGE_PLANNER_SWITCH_H
+#define _ZEABUS_LIBRARY_SUBSCRIBER_MESSAGE_PLANNER_SWITCH_H
+
+
+#endif
