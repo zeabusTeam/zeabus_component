d3fdfe5e0e34d3ae74fbfb820b43f8a0436b6fd7
Supasan Komonlit <supasan_224@hotmail.com>
2019-06-27 12:07:18 +0700

    Add part connect mission

--- zeabus_manager/src/switch_manager.py
+++ zeabus_manager/src/switch_manager.py
@@ -13,6 +13,8 @@ from __future__ import print_function
 import rospy
 from zeabus.control.command_interfaces import CommandInterfaces
 from zeabus_utility.msg import MessagePlannerSwitch
+from zeabus_utility.srv import SendBool
+from std_msgs.msg import Header
 
 _SWITCH_MANAGER_PRINT_RECEIVED_ = True
 
@@ -22,12 +24,18 @@ class SwitchManager:
 
         topic_subscribe_switch = rospy.get_param( '~topic_planer_switch' , "/planner_switch")
 
+        self.header = Header()
+        self.header.frame_id = "mission"
+
         self.limit_process = rospy.get_param( '~limit_process' , 200 )
 
         self.control_status = False # Close control now
         self.control = CommandInterfaces( "manager" )
         self.control.publish_data( "manager have deactive all axies of control" )
         self.control.deactivate( ("x" , "y" , "z" , "roll" , "pitch" , "yaw") )
+
+        # We have to setup client doing task
+        self.client_strategy( '/mission/strategy' , SendBool() )
 
         # First time we must to deactive all freedom of control
         self.count_switch = 0
@@ -56,8 +64,12 @@ class SwitchManager:
             if( self.count_switch == 0 ):
                 self.control.publish_data( "manage now switch tell me time to shutdown" )
                 self.control.deactivate( ("x" , "y" , "z" , "roll" , "pitch" , "yaw" ) )
+                self.header.stamp = rospy.get_rostime()
+                self.client_strategy( self.header , False )
         else: # Now control already status close
             if( self.count_switch == self.limit_process ):
                 self.control.publish_data( "manage now switch tell me to open all" )
                 self.control.reset_state()
                 self.control.activate( ("x", "y" , "z" , "roll" , "pitch" , "yaw" ) )
+                self.header.stamp = rospy.get_rostime()
+                self.client_strategy( self.header , True )
