8375a84ed924a0087f17a9a1079be7d171551af4
Robot <zeabus.auv@gmail.com>
2019-06-28 19:51:54 +0700

    Edit parameter for test

--- zeabus_control/include/zeabus_control/fuzzy/y_parameter.hpp
+++ zeabus_control/include/zeabus_control/fuzzy/y_parameter.hpp
@@ -27,18 +27,18 @@ namespace y_parameter
 
     // Error is range of error to decision 0 1 2 3 by use 3 value. 
     // Data is vector type
-    const std::array< double , 3 > ERROR_RULE = { 0.05 , 0.5 , 2 };
+    const std::array< double , 3 > ERROR_RULE = { 0.05 , 1 , 6 };
     
     // Diff is range to decision about velocity error will be 0 1 2 3 by use 3 value
     // Data is vertor type
-    const std::array< double , 3 > DIFF_RULE = { 0.01 , 0.10 , 0.20 };
+    const std::array< double , 3 > DIFF_RULE = { 0.005 , 0.01 , 0.2 };
     
     // Force is range to decision now you have active force what range
     // member 3 (indent 0) use to decision that is limit of force we can do
     //  Output will decision to 0 1 2 3 4 by use 4 value
     //  If over member 3 (indent 0) we will not addition force
     //  Data is vector type
-    const std::array< double , 4 > FORCE_RULE = { 0.4 , 1.5 , 3 , 5 };
+    const std::array< double , 4 > FORCE_RULE = { 0.1 , 0.5 , 1 , 4 };
     
     // About condition to convert crisp_set to fuzzy_set we will use this condition
     //  if( abs( intput ) < rule[0] ) output = 0
@@ -52,7 +52,7 @@ namespace y_parameter
     // If output is 1 2 will add by DEFUZZY_RULE member 0 1
     // If output is 3 4 will add by DEFUZZY_RULE member 2
     // Data is verctor type
-    const std::array< double , 5 > DEFUZZY_RULE = {0.01 , 0.02 , 0.04, 0.08, 0.16};
+    const std::array< double , 5 > DEFUZZY_RULE = { 0.01, 0.02, 0.04, 0.8, 0.12};
     
     // Offset is value to do and don't have affect with robot in real time always
     const double OFFSET = 0.0;
--- zeabus_control/include/zeabus_control/fuzzy/yaw_parameter.hpp
+++ zeabus_control/include/zeabus_control/fuzzy/yaw_parameter.hpp
@@ -26,22 +26,18 @@ namespace yaw_parameter
 {
     // Error is range of error to decision 0 1 2 3 by use 3 value. 
     // Data is vector type
-    const std::array< double , 3 > ERROR_RULE = { 0.1 , 1 , 2 };
+    const std::array< double , 3 > ERROR_RULE = { 0.08 , 0.4 , 0.6 };
     
     // Diff is range to decision about velocity error will be 0 1 2 3 by use 3 value
     // Data is vertor type
-    const std::array< double , 3 > DIFF_RULE = { 0.01 , 0.05 , 0.1 };
+    const std::array< double , 3 > DIFF_RULE = { 0.005 , 0.05 , 0.12 };
     
     // Force is range to decision now you have active force what range
     // member 3 (indent 0) use to decision that is limit of force we can do
     //  Output will decision to 0 1 2 3 4 by use 4 value
     //  If over member 3 (indent 0) we will not addition force
     //  Data is vector type
-    const std::array< double , 4 > FORCE_RULE = { 0.02 , 0.1 , 1, 2 };
-    
-    // About condition to convert crisp_set to fuzzy_set we will use this condition
-    //  if( abs( intput ) < rule[0] ) output = 0
-    //  else if( abs(input) < rule[1] ) output = 1
+    const std::array< double , 4 > FORCE_RULE = { 0.03 , 0.08 , 0.16 , 1};
     //  else if( abs(input) < rule[2] ) output = 2
     //  else output = 3 
 
@@ -51,11 +47,83 @@ namespace yaw_parameter
     // If output is 1 2 will add by DEFUZZY_RULE member 0 1
     // If output is 3 4 will add by DEFUZZY_RULE member 2
     // Data is verctor type
-    const std::array< double , 5 > DEFUZZY_RULE = {0.01 , 0.02 , 0.04, 0.16, 0.64 };
+    const std::array< double , 5 > DEFUZZY_RULE = {0.01 , 0.03 , 0.04, 0.06, 0.10 };
     
     // Offset is value to do and don't have affect with robot in real time always
     const double OFFSET = 0.0;
     
+    const std::array< std::array< std::array < int , 7 >, 7 >, 7 > FUZZY_RULE = 
+    {
+        // CASE fuzzy force = -3
+        //  -3  -2  -1  +0  +1  +2  +3  = CASE error fuzzy
+            +3, +3, +3, +4, +3, +3, +3, // CASE diff fuzzy = -3
+            +2, +2, +2, +4, +2, +2, +2, // CASE diff fuzzy = -2
+            +0, +0, +0, +4, +2, +2, +3, // CASE diff fuzzy = -1
+            -0, -0, -0, +0, +1, +1, +1, // CASE diff fuzzy = -0
+            -0, -0, -0, -0, +0, -0, -0, // CASE diff fuzzy = +1
+            -0, -0, -0, -0, -0, -0, -0, // CASE diff fuzzy = +2
+            -0, -0, -0, -0, -0, -0, -0, // CASE diff fuzzy = +3
+        
+        // CASE fuzzy force = -2
+        //  -3  -2  -1  +3  +1  +2  +3  = CASE error fuzzy
+            +2, +3, +4, +4, +4, +4, +4, // CASE diff fuzzy = -3
+            +2, +2, +3, +3, +4, +4, +4, // CASE diff fuzzy = -2
+            +0, +0, +0, +3, +3, +3, +4, // CASE diff fuzzy = -1
+            -1, -1, -1, +0, +2, +2, +1, // CASE diff fuzzy = -0
+            -2, -2, -2, -3, +0, -1, -1, // CASE diff fuzzy = +1
+            -3, -2, -2, -3, -1, -1, -1, // CASE diff fuzzy = +2
+            -4, -4, -3, -3, -2, -2, -2, // CASE diff fuzzy = +3
+        
+        // CASE fuzzy force = -1
+        //  -3  -2  -1  +0  +1  +2  +3  = CASE error fuzzy
+            +2, +2, +2, +2, +2, +2, +3, // CASE diff fuzzy = -3
+            +2, +2, +2, +2, +2, +2, +2, // CASE diff fuzzy = -2
+            +0, +0, +0, +1, +1, +1, +2, // CASE diff fuzzy = -1
+            -1, -1, -1, +0, +1, +1, +1, // CASE diff fuzzy = -0
+            -2, -2, -1, -2, +0, -1, -1, // CASE diff fuzzy = +1
+            -3, -2, -2, -2, -2, -1, +1, // CASE diff fuzzy = +2
+            -3, -3, -2, -4, -3, -2, +1, // CASE diff fuzzy = +3
+        
+        // CASE fuzzy force = 0
+        //  -3  -2  -1  +0  +1  +2  +3  = CASE error fuzzy
+            +1, +2, +3, +4, +4, +4, +4, // CASE diff fuzzy = -3
+            +0, +2, +3, +3, +3, +3, +4, // CASE diff fuzzy = -2
+            -1, +0, +0, +1, +2, +3, +3, // CASE diff fuzzy = -1
+            -2, -2, -1, +0, +1, +2, +3, // CASE diff fuzzy = -0
+            -3, -3, -2, -1, +0, +0, +1, // CASE diff fuzzy = +1
+            -4, -3, -3, -3, -3, -2, +0, // CASE diff fuzzy = +2
+            -4, -4, -4, -4, -3, -2, -1, // CASE diff fuzzy = +3
+        
+        // CASE fuzzy force = +1
+        //  -3  -2  -1  +0  +1  +2  +3  = CASE error fuzzy
+            +1, +2, +3, +4, +4, +4, +5, // CASE diff fuzzy = -3
+            +1, +1, +2, +3, +3, +3, +4, // CASE diff fuzzy = -2
+            +1, +1, +0, +1, +2, +2, +3, // CASE diff fuzzy = -1
+            -2, -2, -2, +0, +1, +1, +1, // CASE diff fuzzy = -0
+            -4, -3, -2, -2, +0, -1, -1, // CASE diff fuzzy = +1
+            -4, -3, -3, -3, -2, -1, -1, // CASE diff fuzzy = +2
+            -5, -4, -4, -4, -3, -2, -1, // CASE diff fuzzy = +3
+        
+        // CASE fuzzy force = +2
+        //  -3  -2  -1  +0  +1  +2  +3  = CASE error fuzzy
+            +2, +2, +2, +3, +3, +4, +4, // CASE diff fuzzy = -3
+            +1, +1, +1, +1, +2, +2, +3, // CASE diff fuzzy = -2
+            +1, +1, +0, +1, +2, +2, +2, // CASE diff fuzzy = -1
+            -2, -1, -2, +0, +1, +1, +1, // CASE diff fuzzy = -0
+            -2, -2, -3, -3, +0, +0, +0, // CASE diff fuzzy = +1
+            -3, -3, -3, -3, -2, -2, -1, // CASE diff fuzzy = +2
+            -3, -3, -3, -3, -2, -2, -1, // CASE diff fuzzy = +3
+        
+        // CASE fuzzy force = +3
+        //  -3  -2  -1  +0  +1  +2  +3  = CASE error fuzzy
+            +2, +2, +3, +6, +4, +6, +6, // CASE diff fuzzy = -3
+            +1, +1, +2, +6, +4, +6, +6, // CASE diff fuzzy = -2
+            +1, +1, +0, +6, +4, +5, +1, // CASE diff fuzzy = -1
+            -2, -1, -1, +0, +1, +1, +1, // CASE diff fuzzy = -0
+            -2, -2, -2, -3, +0, +0, +0, // CASE diff fuzzy = +1
+            -3, -3, -3, -3, +0, +0, +0, // CASE diff fuzzy = +2
+            -3, -3, -3, -3, -0, +0, +0  // CASE diff fuzzy = +3
+    };
 } // namespace roll_parameter
 
 } // namespace fuzzy 
