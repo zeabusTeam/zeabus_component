cd00f4a22a97dd3f6e2e8e63eebad709c57add8d
Supasan Komonlit <supasan_224@hotmail.com>
2019-07-05 14:08:20 +0700

    Updated constant

--- zeabus_library_python/src/zeabus/vision/analysis_constant.py
+++ zeabus_library_python/src/zeabus/vision/analysis_constant.py
@@ -15,4 +15,4 @@ BUOY_AREA = 0.0001 # Range is 0 - 1 but this mission have area lowest
 # Constant for estimate gate value
 GATE_NEAR = 4 # Distannce between robot and lenth when length is max ( 200 )
 GATE_RATIO = ( 1 / 100 ) * 2 # Ratio of distance per legth
-GATE_LENGTH = 2 # Ditance of x length range 0 to 200
+GATE_LENGTH = 200 # Ditance of x length range 0 to 200
