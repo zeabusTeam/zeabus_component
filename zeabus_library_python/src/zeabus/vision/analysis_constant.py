9efc684b6175e226cadfa547ced2a61d1e7ef50d
Supasan Komonlit <supasan_224@hotmail.com>
2019-07-05 11:00:15 +0700

    Adding control control

--- zeabus_library_python/src/zeabus/vision/analysis_constant.py
+++ zeabus_library_python/src/zeabus/vision/analysis_constant.py
@@ -11,3 +11,8 @@
 # Constant for filter parameter of buoy mission
 BUOY_SCORE = 50 # Range is 0 - 100
 BUOY_AREA = 0.0001 # Range is 0 - 1 but this mission have area lowest
+
+# Constant for estimate gate value
+GATE_NEAR = 4 # Distannce between robot and lenth when length is max ( 200 )
+GATE_RATIO = ( 1 / 100 ) * 2 # Ratio of distance per legth
+GATE_LENGTH = 2 # Ditance of x length range 0 to 200
