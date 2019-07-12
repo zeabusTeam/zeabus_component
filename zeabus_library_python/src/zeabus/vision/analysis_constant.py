392b834667c4f5347caed2e1954bc43a83e34daf
SupasanKomonlit <supasan_224@hotmail.com>
2019-07-13 04:34:31 +0700

    Update parameter detection

--- zeabus_library_python/src/zeabus/vision/analysis_constant.py
+++ zeabus_library_python/src/zeabus/vision/analysis_constant.py
@@ -11,7 +11,7 @@
 # Constant for filter parameter of buoy mission
 BUOY_SCORE = 50 # Range is 0 - 100
 BUOY_AREA = 0.0001 # Range is 0 - 1 but this mission have area lowest
-BUOY_MINIMUM_FOUND = 1
+BUOY_MINIMUM_FOUND = 2
 
 # Constant for estimate gate value
 GATE_NEAR = 2.0 # Distannce between robot and lenth when length is max ( 200 )
