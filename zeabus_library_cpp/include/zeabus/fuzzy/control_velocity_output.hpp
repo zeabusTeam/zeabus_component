771ec79bae872543381f20721f88f62b2bad812d
SupasanKomonlit <supasan_224@hotmail.com>
2019-07-23 20:57:44 +0700

    Updated new control

--- zeabus_library_cpp/include/zeabus/fuzzy/control_velocity_output.hpp
+++ zeabus_library_cpp/include/zeabus/fuzzy/control_velocity_output.hpp
@@ -5,11 +5,11 @@
 
 double input_error[3][5] = {    {0.09, 0.1, 0.3, 0.9},
                                 {0.09, 0.2, 0.5, 1.0},
-                                {0.09, 0.15, 0.2, 0.9}   };
+                                {0.09, 0.2, 0.5, 1.0}   };
 
-double output_force[3][6] = {   {0.0, 0.05, 0.1, 0.2, 0.4},
+double output_force[3][6] = {   {0.0, 0.05, 0., 0.2, 0.4},
                                 {0.0, 0.03, 0.06, 0.12, 0.5},
-                                {0.0, 0.005, 0.05, 0.1, 0.5}    };
+                                {0.0, 0.001, 0.005, 0.01, 0.05}    };
 
 double force[6] = {0, 0, 0, 0, 0, 0};
 
@@ -19,20 +19,24 @@ int fuzzy[5][5] = {  {1, 1, 2, 2, 3},
                             {-1, 0, 1, 1, 2},
                             {-1, -1, 0, 1, 1}   };
 
-// double pre_error = 0;
+int fuzzy_yaw[5][5] = { {1, 2, 2, 3,3},{1,2,2,3,3},{0,1,2,2,3},{0,0,1,2,2}  };
 
 int error_range(double error,int type);
 
-double find_acceleration(double error);
+int pre_output_range(double pre_output, int type);
 
-int acceleration_range(double acceleration_value);
-
-int choose_fuzzy(int error_range_value, int acceleration_range_value);
+int choose_fuzzy(int error_range_value, int pre_output_range_value);
 
 double fuzzy_range(int choose_fuzzy_value, int type, double error);
 
 double add_force(double fuzzy_range_value,int axis);
 
 double choose_type(int axis);
+
+double check(double output, int type, int axis);
+
+void copyarray(int type);
+
+void copyarray_yaw(int type);
 
 double run_system(double error, int axis, bool mask);
--- zeabus_library_cpp/src/zeabus/fuzzy/control_velocity_output.cpp
+++ zeabus_library_cpp/src/zeabus/fuzzy/control_velocity_output.cpp
@@ -1,6 +1,6 @@
-#include <zeabus/fuzzy/control_velocity.hpp>
+#include <zeabus/fuzzy/control_velocity_output.hpp>
 
-double pre_error = 0;
+double pre_output = 0;
 
 int error_range(double error, int type)
 {
@@ -28,43 +28,36 @@ int error_range(double error, int type)
     return error_range_value;
 }
 
-double find_acceleration(double error)
+int pre_output_range(double pre_output, int type)
 {
-    int acceleration_value = 0;
-    acceleration_value = pre_error - error;
-    return acceleration_value;
+    int pre_output_range_value = 0;
+    if(pre_output == output_force[type][0])
+    {
+        pre_output_range_value = 0;
+    }
+    else if(pre_output == output_force[type][1])
+    {
+        pre_output_range_value = 1;
+    }
+    else if(pre_output == output_force[type][2])
+    {
+        pre_output_range_value = 2;
+    }
+    else if(pre_output == output_force[type][3])
+    {
+        pre_output_range_value = 3;
+    }
+    else if(pre_output == output_force[type][4])
+    {
+        pre_output_range_value = 4;
+    }
+    return pre_output_range_value;
 }
 
-int acceleration_range(double acceleration_value)
-{
-    int acceleration_range_value = 0;
-    if(acceleration_value >= -10)
-    {
-        acceleration_range_value = 0;
-    }
-    else if(acceleration_value >= -5)
-    {
-        acceleration_range_value = 1;
-    }
-    else if(acceleration_value >= 0)
-    {
-        acceleration_range_value = 2;
-    }
-    else if(acceleration_value >= 5)
-    {
-        acceleration_range_value = 3;
-    }
-    else if(acceleration_value >= 10)
-    {
-        acceleration_range_value = 4;
-    }
-    return acceleration_range_value;
-}
-
-int choose_fuzzy(int error_range_value, int acceleration_range_value)
+int choose_fuzzy(int error_range_value, int pre_output_range_value)
 {
     int choose_fuzzy_value = 0;
-    choose_fuzzy_value = fuzzy[acceleration_range_value][error_range_value];
+    choose_fuzzy_value = fuzzy[pre_output_range_value][error_range_value];
     return choose_fuzzy_value;
 }
 
@@ -78,8 +71,12 @@ double fuzzy_range(int choose_fuzzy_value, int type, double error)
 double add_force(double fuzzy_range_value, int axis)
 {
     double add_force_value = 0;
+    std::cout << "force[axis]: " << force[axis] << std::endl;
+    std::cout << "fuzzy_range_value: " << fuzzy_range_value << std::endl;
     add_force_value = force[axis] + fuzzy_range_value;
-    return add_force_value;
+    force[axis] = add_force_value;
+    std::cout << "force[axis]: " << force[axis] << std::endl;
+    return force[axis];
 }
 
 double choose_type(int axis)
@@ -100,38 +97,64 @@ double choose_type(int axis)
     return type;
 }
 
-int check(double output, int type)
+double check(double output, int type, int axis)
 {
     if(type == 0)
     {
-        if(output >= 10)
+        if(output >= 10 || output <= -10)
         {
-            output = 10;
+            force[axis] = std::copysign(10, output);
         }
     }
     else if(type == 1)
     {
-        if(output >= 8)
+        if(output >= 8 || output <= -8)
         {
-            output = 8;
+            force[axis] = std::copysign(8, output);
         }
     }
     else if(type == 2)
     {
-        if(output >= 3)
+        if(output >= 1 || output <= -1)
         {
-            output = 3;
+            force[axis] = std::copysign(1, output);
         }
     }
-    return output;
+    std::cout << "check_output: " << force[axis] << std::endl;
+    return force[axis];
+}
+
+int fuzzy_use[5][5] = {};
+
+void copyarrary(int type)
+{
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            fuzzy_use[i][j] = fuzzy[i][j];
+        }
+    }
+    return;
+}
+
+void copyarrary_yaw(int type)
+{
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            fuzzy_use[i][j] = fuzzy_yaw[i][j];
+        }
+    }
+    return;
 }
 
 double run_system(double error, int axis, bool mask)
 {
     int type = 0;
     int error_range_value_out = 0;
-    double acceleration_value_out = 0;
-    int acceleration_range_value_out = 0;
+    int pre_output_range_value = 0;
     int choose_fuzzy_value_out = 0;
     double fuzzy_range_value_out = 0;
     double add_force_value_out = 0;
@@ -141,22 +164,29 @@ double run_system(double error, int axis, bool mask)
         return force[axis];
     }
     std::cout << "error: " << error << std::endl;
+    std::cout << std::endl;
     type = choose_type(axis);
     std::cout << "type: " << type << std::endl;
+    //if(type == 0)
+    //{
+    //    copyarray(type);
+    //}
+    //else if(type == 2)
+    //{
+    //    copyarray_yaw(type);
+    //}
     error_range_value_out = error_range(error, type);
     std::cout << "error_range_value_out: " << error_range_value_out << std::endl;
-    acceleration_value_out = find_acceleration(error);
-    std::cout << "acceleration_value_out: " << acceleration_value_out << std::endl;
-    acceleration_range_value_out = acceleration_range(acceleration_value_out);
-    std::cout << "acceleration_range_value_out: " << acceleration_range_value_out << std::endl;
-    choose_fuzzy_value_out = choose_fuzzy(error_range_value_out, acceleration_range_value_out);
+    pre_output_range_value = pre_output_range(pre_output, type);
+    std::cout << "pre_output_range_value: " << pre_output_range_value << std::endl;
+    choose_fuzzy_value_out = choose_fuzzy(error_range_value_out, pre_output_range_value);
     std::cout << "choose_fuzzy_value_out: " << choose_fuzzy_value_out << std::endl;
     fuzzy_range_value_out = fuzzy_range(choose_fuzzy_value_out, type, error);
+    pre_output = fuzzy_range_value_out;
     std::cout << "fuzzy_range_value_out: " << fuzzy_range_value_out << std::endl;
     add_force_value_out = add_force(fuzzy_range_value_out, axis);
     std::cout << "add_force_value_out: " << add_force_value_out << std::endl;
-    pre_error = error;
-    std::cout << "pre_error: " << pre_error << std::endl;
-    add_force_value_out = check(add_force_value_out, type);
+    add_force_value_out = check(add_force_value_out, type, axis);
+    std::cout << std::endl;
     return add_force_value_out;
 }
